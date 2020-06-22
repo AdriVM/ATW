4461dd58ba24602ce1a2a449090f71f3a9a9177f
Adrián <avazmir1337@gmial.com>
2020-06-22 16:44:09 +0200

    Theme ATW

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+(function ($, root, undefined) {
+	
+	$(function () {
+		
+		'use strict';
+		
+		jQuery('.gallery a').each(function() {
+			jQuery(this).attr({'data-lightbox':'galeria'});
+		});
+
+		jQuery('ul.slider').bxSlider({
+			mode: 'fade',
+			auto: true,
+			pager:false
+		});
+
+		jQuery('.single-tours header nav ul li:contains("Tours")').addClass('current_page_item');
+
+		jQuery('.single-post header nav ul li:contains("Consejos")').addClass('current_page_item');
+	});
+	
+})(jQuery, this);
