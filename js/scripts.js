453bae18ceb31817385a56145cd90f944204bc93
Adrián <avazmir1337@gmial.com>
2020-06-23 14:33:06 +0200

    Modificación del menú principal para que marque como activo la parte de consejos cuando estamos dentro de las páginas de autor o categoría.

--- js/scripts.js
+++ js/scripts.js
@@ -22,6 +22,10 @@
 			jQuery('.single-tours header nav ul li:contains("Tours")').addClass('current_page_item');
 	
 			jQuery('.single-post header nav ul li:contains("Consejos")').addClass('current_page_item');
+
+			jQuery('.category header nav ul li:contains("Consejos")').addClass('current_page_item');
+
+			jQuery('.author header nav ul li:contains("Consejos")').addClass('current_page_item');
 		
 		
 	});
