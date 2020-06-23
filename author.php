8ae03c0c60ab773a7ef7fb0590702671699f6e51
Adrián <avazmir1337@gmial.com>
2020-06-23 14:31:47 +0200

    Modificación de las plantillas de autor y categoría. (Se accede por la parte de consejos)

--- author.php
+++ author.php
@@ -1,77 +1,85 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+<main role="main">
+    <!-- section -->
+    <section>
 
-		<?php if (have_posts()): the_post(); ?>
+        <?php if (have_posts()): the_post(); ?>
 
-			<h1><?php _e( 'Author Archives for ', 'html5blank' ); echo get_the_author(); ?></h1>
+        <h1><span><?php _e( 'Todas las entradas de: ', 'html5blank' ); echo get_the_author(); ?></span></h1>
 
-		<?php if ( get_the_author_meta('description')) : ?>
+        <?php if ( get_the_author_meta('description')) : ?>
 
-		<?php echo get_avatar(get_the_author_meta('user_email')); ?>
+        <?php echo get_avatar(get_the_author_meta('user_email')); ?>
 
-			<h2><?php _e( 'About ', 'html5blank' ); echo get_the_author() ; ?></h2>
+        <h2><?php _e( 'Sobre: ', 'html5blank' ); echo get_the_author() ; ?></h2>
 
-			<?php echo wpautop( get_the_author_meta('description') ); ?>
+        <?php echo wpautop( get_the_author_meta('description') ); ?>
 
-		<?php endif; ?>
+        <?php endif; ?>
 
-		<?php rewind_posts(); while (have_posts()) : the_post(); ?>
+        <?php rewind_posts(); while (have_posts()) : the_post(); ?>
 
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <!-- article -->
+        <article id="post-<?php the_ID(); ?>" <?php post_class('clear'); ?>>
 
-				<!-- post thumbnail -->
-				<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-						<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
-					</a>
-				<?php endif; ?>
-				<!-- /post thumbnail -->
+            <!-- post title -->
+            <h2>
+                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+            </h2>
+            <!-- /Post title -->
 
-				<!-- post title -->
-				<h2>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-				</h2>
-				<!-- /Post title -->
+            <div class="grid1-3">
+                <!-- post thumbnail -->
+                <?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+                <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+                    <?php the_post_thumbnail('mediano'); // Declare pixel size you need inside the array ?>
+                </a>
+                <?php endif; ?>
+                <!-- /post thumbnail -->
+            </div>
 
-				<!-- post details -->
-				<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-				<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-				<span class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-				<!-- /post details -->
+            <div class="grid2-3">
 
-				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+                <!-- post details -->
+                <span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+                <span class="author"><?php _e( 'Published by', 'html5blank' ); ?>
+                    <?php the_author_posts_link(); ?></span>
+                <span
+                    class="comments"><?php comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
+                <!-- /post details -->
 
-				<br class="clear">
+                <?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
 
-				<?php edit_post_link(); ?>
+                <br class="clear">
 
-			</article>
-			<!-- /article -->
+                <?php edit_post_link(); ?>
 
-		<?php endwhile; ?>
+            </div>
 
-		<?php else: ?>
+        </article>
+        <!-- /article -->
 
-			<!-- article -->
-			<article>
+        <?php endwhile; ?>
 
-				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+        <?php else: ?>
 
-			</article>
-			<!-- /article -->
+        <!-- article -->
+        <article>
 
-		<?php endif; ?>
+            <h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
 
-			<?php get_template_part('pagination'); ?>
+        </article>
+        <!-- /article -->
 
-		</section>
-		<!-- /section -->
-	</main>
+        <?php endif; ?>
 
-<?php get_sidebar(); ?>
+        <?php get_template_part('pagination'); ?>
+
+    </section>
+    <!-- /section -->
+</main>
+
+<?php //get_sidebar(); ?>
 
 <?php get_footer(); ?>
